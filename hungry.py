b6972e01bdee44293e46dc12fa2214cda0243414
goos3bumps <goosebumps0@protonmail.com>
2019-01-04 09:36:37 +0530

    fry

--- hungry.py
+++ hungry.py
@@ -5,6 +5,7 @@ hungry = input("are you hungry? ")
 if hungry=="yes":
                print("eat jalebi")
                print("eat pizza")
+               print("eat fry")
 else:
      print("do your homework")
 
